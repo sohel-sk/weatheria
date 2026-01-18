a04a40ca7b8cb416f4df5384b5168ed1ddda10b5
sohel-sk <sohelsheikh9623@gmail.com>
2026-01-18 13:49:22 +0530

    feat : weather service update

--- lib/src/core/services/weather_service.dart
+++ lib/src/core/services/weather_service.dart
@@ -6,22 +6,40 @@ final String _apiKey = ApiConstants.apiKey;
 final String _baseUrlWeather = ApiConstants.baseUrlWeather;
 class WeatherService {
 
-  Future<WeatherModel> fetchWeather(String city) async {
-    final response = await http.get(
-      Uri.parse("$_baseUrlWeather/currentConditions:lookup?key=$_apiKey&location.latitude=LATITUDE&location.longitude=LONGITUDE"),
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception("Failed to load weather");
+  Future<WeatherModel> fetchCurrentConditions({
+    required double lat,
+    required double lng,
+  }) async {
+    try {
+      final url =
+          "$_baseUrlWeather/currentConditions:lookup?key=$_apiKey&location.latitude=$lat&location.longitude=$lng";
+      
+      final response = await http.get(Uri.parse(url));
+      
+      if (response.statusCode != 200) {
+        throw Exception("Failed to fetch current conditions");
+      }
+      
+      final data = json.decode(response.body);
+      
+      return WeatherModel(
+        description: data['weatherCondition']['description']['text'],
+        iconUrl: data['weatherCondition']['iconBaseUri'],
+        temperature: data['temperature']['degrees'].toDouble(),
+        feelsLike:
+            data['feelsLikeTemperature']['degrees'].toDouble(),
+        humidity: data['relativeHumidity'],
+        uvIndex: data['uvIndex'],
+        pressure:
+            data['airPressure']['meanSeaLevelMillibars'].toDouble(),
+        windSpeed: data['wind']['speed']['value'].toDouble(),
+        windDirection: data['wind']['direction']['cardinal'],
+        visibility: data['visibility']['distance'],
+        isDaytime: data['isDaytime'],
+      );
+    } on Exception catch (e) {
+      // TODO
+      throw Exception("Error fetching current conditions: $e");
     }
-
-    final data = json.decode(response.body);
-
-    return WeatherModel(
-      city: data['name'],
-      temperature: data['main']['temp'].round(),
-      condition: data['weather'][0]['description'],
-      aqi: 120, // placeholder (AQI needs separate API)
-    );
   }
 }
